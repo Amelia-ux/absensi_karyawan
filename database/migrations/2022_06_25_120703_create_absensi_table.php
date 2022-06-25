2e3a4293b57d8bf508351986866f8e626a39e263
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-06-25 20:14:29 +0700

    migration

--- database/migrations/2022_06_25_120703_create_absensi_table.php
+++ database/migrations/2022_06_25_120703_create_absensi_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAbsensiTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('absensi', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('foto');
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger("user_id")->nullable();
+            $table->boolean("absen");
+            $table->date("tgl");
             $table->timestamps();
+
+            // $table->foreign('user_id')->reference('id')->on('users');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('absensi');
     }
 }
---
+++ database/migrations/2022_06_25_125541_relasi_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RelasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->reference('id')->on('role');
+        });
+        Schema::table('absensi', function(Blueprint $table){
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->reference('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->dropColumn(['role_id']);
+            $table->dropForeign(['role_id']);
+        });
+        Schema::table('absensi', function(Blueprint $table){
+            $table->dropColumn(['user_id']);
+            $table->dropForeign(['user_id']);
+        });
+    }
+}
