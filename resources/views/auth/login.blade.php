4a6f4860465e523829a16ab710f7b987df26188f
Dka-11 <adikaahmad05@gmail.com>
2022-06-27 10:57:39 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="login-form">
-                            <h4>Login</h4>
-                            <form method="post" action="{{ route('login') }}">
-                                @csrf
-                                <div class="form-group">
-                                    <label>Email address</label>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                                </div>
+    <div class="login-form">
+        <h4>Login</h4>
+        <form method="post" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <label>Email address</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                    value="{{ old('email') }}" required autocomplete="email" autofocus>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                    name="password" required autocomplete="current-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
 
-                                <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Sign in</button>
-                                <div class="register-link m-t-15 text-center">
-                                    <p>Don't have account ? <a href="#"> Sign Up Here</a></p>
-                                </div>
-                            </form>
-                        </div>
+            <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Sign in</button>
+        </form>
+    </div>
 @endsection
