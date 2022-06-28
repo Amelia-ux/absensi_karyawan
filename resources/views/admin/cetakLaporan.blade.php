da22bef0798426391409867cdd58e746cd99a2b9
Dka-11 <adikaahmad05@gmail.com>
2022-06-28 20:53:12 +0700

    edit views

---
+++ resources/views/admin/cetakLaporan.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cetak KHS</title>
+</head>
+
+<body>
+    <h3 class="text-center mt-4">PT. TRIO Kelinci </h3>
+    <h2 class="text-center mt-5 mb-4">Laporan Absensi Karyawan</h2>
+    <strong>Email: </strong>{{ $user->email }}<br>
+    <strong>Nama: </strong>{{ $user->name }}<br>
+    <table class="table table-striped mt-2">
+        <th>Absensi</th>
+        <th>Keterangan</th>
+        @foreach ($absensi as $a)
+            <tr>
+                <td>{{ $a->ket_id }}</td>
+                <td>{{ $a->ket_id->keterangan }}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
--- resources/views/admin/editU.blade.php
+++ resources/views/admin/editU.blade.php
@@ -7,38 +7,42 @@
             </div>
             <div class="card-body">
                 @if ($errors->any())
-                <div class="alert alert-danger">
-                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                 @endif
                 <form method="post" action="/admin/updateU/{{ $user->id }}" id="myForm">
                     @csrf
                     @method('PUT')
                     <div class="form-group">
+                        <label for="foto">Foto</label>
+                        <input type="file" name="foto" id="Foto" value="{{ $user->foto }}">
+                    </div>
+                    <div class="form-group">
                         <label for="Email">Email</label>
-                        <input type="text" name="email" class="form-control" id="email" value="{{ $user->email }}" aria-describedby="email" >
+                        <input type="text" name="email" class="form-control" id="email"
+                            value="{{ $user->email }}" aria-describedby="email">
                     </div>
                     <div class="form-group">
                         <label for="Name">Nama</label>
-                        <input type="text" name="name" class="form-control" id="name" value="{{ $user->name }}" ariadescribedby="name" >
+                        <input type="text" name="name" class="form-control" id="name"
+                            value="{{ $user->name }}" ariadescribedby="name">
                     </div>
                     <div class="form-group">
                         <label for="Role">Role</label>
                         <select name="role" id="role" class="form-control">
                             @foreach ($role as $roles)
-                            <option value="{{ $roles->id }}" {{ $user->role_id == $roles->id ? 'selected' : '' }}>{{ $roles->nama_role }}</option>
+                                <option value="{{ $roles->id }}"
+                                    {{ $user->role_id == $roles->id ? 'selected' : '' }}>{{ $roles->nama_role }}
+                                </option>
                             @endforeach
-                        </select>   
+                        </select>
                     </div>
-                    {{-- <div class="form-group">
-                        <label for="foto">Foto</label> <br>         
-                        <input type="file" name="foto" id="Foto" value="{{ $Mahasiswa->foto }}">
-                    </div> --}}
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
